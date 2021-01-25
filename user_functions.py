34dc51eaed680a641e99a89abcaef180864d3813
Melody Peterson <melodygr@aol.com>
2021-01-25 00:19:47 -0500

    modeling

--- user_functions.py
+++ user_functions.py
@@ -23,6 +23,73 @@ from sklearn.ensemble import BaggingClassifier, RandomForestClassifier, AdaBoost
 from xgboost import XGBClassifier
 
 
+def visualize_time_series(df, name):
+    """Plot time series. Plot annual breakouts for years with all 12 values."""
+    df.plot(figsize = (12,4))
+    plt.title(name)
+    plt.xlabel('Year')
+    plt.ylabel('Median House Price');
+    
+    # Use pandas grouper to group values using annual frequency
+    year_groups = df.groupby(pd.Grouper(freq ='A'))
+
+    # Create a new DataFrame and store yearly values in columns 
+    df_annual = pd.DataFrame()
+
+    # print(list(year_groups))
+    for yr, group in year_groups:
+        if len(group) == 12: # Can only use full years of data
+            df_annual[yr.year] = group.values.ravel()
+    
+    # Plot the yearly groups as subplots
+    df_annual.plot(figsize = (13,20), subplots=True, legend=True);
+
+    # Plot overlapping yearly groups 
+    df_annual.plot(figsize = (15,10), subplots=False, legend=True);
+    
+def visualize_all_series(list_of_df, names):
+    """Plot a list of time series dataframes together with provided names for legend."""
+    df_group = pd.concat(list_of_df, axis=1)
+    df_group.columns = names
+    df_group.plot(figsize = (12,4), subplots=False, legend=True)
+    plt.title('Median House Prices Over Time')
+    plt.xlabel('Year')
+    plt.ylabel('Median House Price')
+    plt.savefig('images/lineplotallzips.png')
+    plt.show();
+
+def stationarity_check(TS):
+    """Calculate rolling statistics and plot against original time series, perform and output Dickey Fuller test."""
+    # Import adfuller
+    from statsmodels.tsa.stattools import adfuller
+    
+    # Calculate rolling statistics
+    roll_mean = TS.rolling(window=24, center=False).mean()
+    roll_std = TS.rolling(window=24, center=False).std()
+        
+    # Perform the Dickey Fuller Test
+    dftest = adfuller(TS)
+    
+    # Plot rolling statistics:
+    fig = plt.figure(figsize=(12,6))
+    plt.plot(TS, color='blue',label='Original')
+    plt.plot(roll_mean.dropna(), color='red', label='Rolling Mean')
+    plt.plot(roll_std.dropna(), color='black', label = 'Rolling Std')
+    plt.legend(loc='best')
+    plt.title('Rolling Mean & Standard Deviation')
+    plt.show(block=False)
+    
+    # Print Dickey-Fuller test results
+    print('Results of Dickey-Fuller Test: \n')
+
+    dfoutput = pd.Series(dftest[0:4], index=['Test Statistic', 'p-value', 
+                                             '#Lags Used', 'Number of Observations Used'])
+    for key,value in dftest[4].items():
+        dfoutput['Critical Value (%s)'%key] = value
+    print(dfoutput)
+    
+    return None
+
 def print_roc(false_positive_rate, true_positive_rate):    
     roc_auc = auc(false_positive_rate, true_positive_rate)
     print('AUC: {}'.format(auc(false_positive_rate, true_positive_rate)))
